856e7d2e8e9328d5d1fc05da086c05822800de3f
alirezatcp <alirezataaty02@gmail.com>
2024-12-10 15:55:39 -0500

    add input-output hints to input-output.py

---
+++ input-output.py
@@ -0,0 +1,9 @@
+my_input = input('Enter your input with ","  : ').split(',') #  1,2,3,4,5
+print(my_input) # ['1', '2', '3', '4', '5']
+
+int_list = [int (x) for x in my_input]
+print(int_list) # [1, 2, 3, 4, 5]
+
+print('hi...', end='')
+print('continiue')
+
