e187676ca2bb44d239abfaf9f2dd9080ccbea30f
alirezatcp <alirezataaty02@gmail.com>
2024-12-10 15:32:51 -0500

    add some hints about lists to lists.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+list1 = list('alireza')
+print(list1) # ['a', 'l', 'i', 'r', 'e', 'z', 'a']
+
+list2 = 'this,is,list,2'.split(',')
+print(list2) # ['this', 'is', 'list', '2']
+
+l1 = [1,2,3]
+l2 = [3,4,5]
+print(l1+l2) # [1, 2, 3, 3, 4, 5]
+print(l1*2) # [1, 2, 3, 1, 2, 3]
+
+print(l1<l2) # compare l1[0] with l2[0] and return True
+print(l1>l2) # False
