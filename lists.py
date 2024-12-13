0c25143434da7be9501cf81802da928ef2a9943f
alirezatcp <alirezataaty02@gmail.com>
2024-12-13 16:15:13 -0500

    add about sorting lists to lists.py

--- lists.py
+++ lists.py
@@ -14,3 +14,11 @@ print(l1>l2) # False
 
 args = [1,2,3,4,5]
 print(*args) # 1 2 3 4 5
+
+list_for_sort = [4, 3, 2, 5, 6]
+print(list_for_sort.sort()) # None
+print(list_for_sort) # [2, 3, 4, 5, 6]
+
+list_for_sort = [4, 3, 2, 5, 6]
+new_list = sorted(list_for_sort) # options: reverce=True (sort reverse), key=len(sort with length. we can write our func too.)
+print(list_for_sort, new_list) # [4, 3, 2, 5, 6] [2, 3, 4, 5, 6]
