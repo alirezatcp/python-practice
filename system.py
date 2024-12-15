3dad91279a683660055973f65a6d078ec8eb7097
alirezatcp <alirezataaty02@gmail.com>
2024-12-15 08:00:56 -0500

    add about sys library to system.py

---
+++ system.py
@@ -0,0 +1,11 @@
+import sys
+
+print(sys.path) # shows pathes we can use in this script. we can add another path with this command (TODO): sys.path.append(r'new_path_directory') 
+
+
+print(__file__) # shows program address
+
+print(__name__) # print program name
+
+print(dir(sys)) # name of all funcs of sys library
+
